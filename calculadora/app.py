ccf59d7df586d99437a4e014798db3d15724301a
Tiago.Dev <shublayder3@gmail.com>
2026-04-25 18:01:21 -0300

    app.py

--- calculadora/app.py
+++ calculadora/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Variável para guardar os números digitados
+
 conteudo_visor = ""
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,18 +12,18 @@ def index():
     if request.method == 'POST':
         botao_apertado = request.form.get('botao')
         
-        # 1. Se apertar o igual, calcula
+      
         if botao_apertado == "=":
             try:
                 conteudo_visor = str(eval(conteudo_visor))
             except:
                 conteudo_visor = "Erro"
         
-        # 2. Se apertar o C, limpa tudo
+       
         elif botao_apertado == "C":
             conteudo_visor = ""
             
-        # 3. Se for número ou sinal, vai grudando no visor
+       
         else:
             conteudo_visor += botao_apertado
 
